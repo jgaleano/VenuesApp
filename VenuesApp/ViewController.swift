59f2ec2bfad589ab6d5d73ffce8a720750ecfe5f
Julio Laptop <JulioC@Julios-MacBook-Pro.local>
2019-01-24 09:54:31 -0500

    Initial commit

---
+++ VenuesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VenuesApp
+//
+//  Created by Julio Laptop on 1/24/19.
+//  Copyright © 2019 Julio Galeano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
