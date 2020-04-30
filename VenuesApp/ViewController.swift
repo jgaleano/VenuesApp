d5cee288330bd4cbd0189c37ca09ac986353f30f
Julio César Galeano García <>
2020-04-30 18:30:45 -0500

    Array sum commit

--- VenuesApp/ViewController.swift
+++ VenuesApp/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sumBtn: UIButton!
+    @IBOutlet weak var sumResultLbl: UILabel!
+    
+    let viewModel = ViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func sumClick(_ sender: Any) {
+        sumResultLbl.text = "\(viewModel.sum(values: [1,2,3,4,5,6,7,8,9]))"
+    }
 }
 
---
+++ VenuesApp/ViewModel/ViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  ViewModel.swift
+//  VenuesApp
+//
+//  Created by Julio César Galeano García on 30/04/20.
+//  Copyright © 2020 Julio Galeano. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel: NSObject {
+    func sum(values: [Int]) -> Int {
+        var result = 0
+        for value in values {
+            result += value
+        }
+        return result
+    }
+}
